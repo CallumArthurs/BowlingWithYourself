1d705fbf98c9161e3ff994304c10fb4ee35d8f47
CallumArthurs <43657846+CallumArthurs@users.noreply.github.com>
2019-06-27 14:10:38 +1000

    Update
    +Pins will get a reference to the ball script if theyget touched by an object with ball tag on it
    +Pins now check if they have been knocked over and add a point
    +Ball tag added to the Ball

---
+++ BowlingWithYourself/Assets/Scripts/PinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinScript : MonoBehaviour
+{
+    private BallController ball;
+    private bool knockedOver = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            ball = collision.gameObject.GetComponent<BallController>();
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Dot(gameObject.transform.up, Vector3.up) < 0.3f && Vector3.Dot(gameObject.transform.up, Vector3.up) > -0.3f && !knockedOver)
+        {
+            ball.ChangeScore(1);
+            knockedOver = true;
+        }
+    }
+}
